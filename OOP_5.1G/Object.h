8909ef74a34d8ef4b2217f6ce7da34e52c20d4cd
Denis Yatsenko <Denys.Yatsenko.ITIS.2020@lpnu.ua>
2021-04-26 23:08:48 +0300

    first commit

---
+++ OOP_5.1G/Object.h
@@ -0,0 +1,27 @@
+//////////////////////////////////////////////////////////////////////////////
+// Object.h
+// заголовний файл Ц визначенн€ класу
+#pragma once
+
+class Object
+{
+private:
+	static unsigned int count;
+
+public:
+	static unsigned int Count()
+	{
+		return count;
+	}
+
+	Object()
+	{
+		count++;
+	}
+
+	~Object()
+	{
+		count--;
+	}
+};
+
